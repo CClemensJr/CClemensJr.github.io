ba059310edb466258114925fdf1a0983660b83cb
Charles Clemens <cclemensjr@gmail.com>
2018-10-09 21:09:57 -0700

    Added instructions for js functionality

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
-
-//Right out what to do here
+//When the user clicks on the resume button, show a copy of the resume pdf in a modal
+//Provide an option to save a copy of the PDF
+//When the user clicks on the projects button, show a carousel showing projects
+//Each carousel item should be a card with an image of the app, the project name, a description of the technologies used, and a description of what the app does.
+//When the user clicks on the card, they are taken to the project site.
 
 'use strict';
